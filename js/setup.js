b4026ae39921af78b11123d3fcf8ce7f1af5b5c2
Anatoly Sadowsky <sadowsky83@gmail.com>
2017-02-05 19:32:59 +0700

    Выполнил задание 13 Учебный проект: Нажатие посохом

--- js/setup.js
+++ js/setup.js
@@ -1,20 +1,76 @@
 'use strict';
 
-// открытие / закрытие блока setup
-
 var openSetup = document.querySelector('.setup-open');
 var closeSetup = document.querySelector('.setup-close');
-var setupBlock = document.querySelector('.setup');
+var setup = document.querySelector('.setup');
+var setupSubmit = document.querySelector('.setup-submit');
 
-// открытие
-openSetup.addEventListener('click', function () {
-  setupBlock.classList.remove('invisible');
+var ENTER_KEY_CODE = 13;
+var ESCAPE_KEY_CODE = 27;
+
+// определение переменной ENTER_KEY_CODE (клавиши Ввод)
+var activateEvent = function(evt) {
+  return evt.keyCode && evt.keyCode === ENTER_KEY_CODE;
+};
+
+// обработка нажатий клавиш в виджете setup
+var setupKeydownHandler = function(evt) {
+  if (evt.keyCode === ESCAPE_KEY_CODE) {
+    setup.classList.add('invisible');
+  }
+};
+
+// открытие виджета setup
+var showSetupWidget = function() {
+  setup.classList.remove('invisible');
+  document.addEventListener('keydown', setupKeydownHandler);
+};
+
+// закрытие виджета setup
+var hideSetupWidget = function() {
+  setup.classList.add('invisible');
+  document.removeEventListener('keydown', setupKeydownHandler);
+};
+
+// изменение ARIA роли
+var statusAriaRole = function(item) {
+  var pressed = (item.getAttribute('aria-pressed') === 'true');
+  item.setAttribute('aria-pressed', !pressed);
+};
+
+// открытие виджета setup по клику
+openSetup.addEventListener('click', function() {
+  showSetupWidget();
+  statusAriaRole(openSetup);
 });
 
-// закрытие
-closeSetup.addEventListener('click', function () {
-  setupBlock.classList.add('invisible');
+// открытие виджета setup по нажатию
+openSetup.addEventListener('keydown', function(evt) {
+  if (activateEvent(evt)) {
+    showSetupWidget();
+    statusAriaRole(openSetup);
+  }
 });
+
+// закрытие виджета setup по клику
+var closeButton = function(nameButton) {
+  nameButton.addEventListener('click', function() {
+    hideSetupWidget();
+    statusAriaRole(nameButton);
+  });
+
+  // закрытие виджета setup по нажатю
+  nameButton.addEventListener('keydown', function(evt) {
+    if (activateEvent(evt)) {
+      hideSetupWidget();
+      statusAriaRole(nameButton);
+    }
+  });
+};
+
+closeButton(closeSetup);
+closeButton(setupSubmit);
+
 
 // изменение цвета фигурки мага и цвета фаербола
 var wizardCoat = document.getElementById('wizard-coat');
@@ -48,28 +104,28 @@ var fireballColors = [
 ];
 
 // случайный выбор цветов из массивов для fill
-var colorFill = function (element, arrayColors) {
+var colorFill = function(element, arrayColors) {
   var changeColorFill = Math.floor(Math.random() * arrayColors.length);
   element.style.fill = arrayColors[changeColorFill];
 };
 
 // выбор случайного цвета для одежды
-wizardCoat.addEventListener('click', function () {
+wizardCoat.addEventListener('click', function() {
   colorFill(wizardCoat, wizardCoatColors);
 });
 
 // выбор случайного цвета для глаз
-wizardEyes.addEventListener('click', function () {
+wizardEyes.addEventListener('click', function() {
   colorFill(wizardEyes, wizardEyesColors);
 });
 
 // случайный выбор цветов из массивов для background
-var colorBackground = function (element, arrayColors) {
+var colorBackground = function(element, arrayColors) {
   var changeColorBackground = Math.floor(Math.random() * arrayColors.length);
   element.style.background = arrayColors[changeColorBackground];
 };
 
 // выбор случайного цвета для фаербола
-fireball.addEventListener('click', function () {
+fireball.addEventListener('click', function() {
   colorBackground(fireball, fireballColors);
 });
